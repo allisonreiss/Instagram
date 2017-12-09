f6b1736a81a82ba1337e0b9757181061958e97e2
Allison Reiss <allisonreiss1@gmail.com>
2017-12-08 21:17:17 -0800

    Setup capture layout

--- Instagram/View Controllers/CaptureViewController.swift
+++ Instagram/View Controllers/CaptureViewController.swift
@@ -8,28 +8,72 @@
 
 import UIKit
 
-class CaptureViewController: UIViewController {
+class CaptureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    private var imagePickerController: UIImagePickerController!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var captionTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+
+        // show the picture on the capture page
+        photoImageView.image = originalImage
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onAddPhoto(_ sender: Any) {
+        print("I see that you tapped yo")
+        
+        //show image picker
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+
+    
+    func resize(photo: UIImage, newSize: CGSize) -> UIImage {
+        let resizeImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        resizeImageView.contentMode = UIViewContentMode.scaleAspectFill
+        resizeImageView.image = photo
+        
+        UIGraphicsBeginImageContext(resizeImageView.frame.size)
+        resizeImageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+    
+    @IBAction func onPost(_ sender: Any) {
+        // Set the caption and photo
+        let caption = captionTextField.text
+        let postPic = resize(photo: self.photoImageView.image!, newSize: CGSize(width:240, height:240))
+        
+        // Post the photo and caption
+        Post.postUserImage(photo: postPic, caption: caption) { (success: Bool, error: Error?) in
+            if success {
+                print("Yayyyy photo posted!")
+                // go back to Home Feed
+                self.tabBarController?.selectedIndex = 0
+            } else {
+                print(error?.localizedDescription ?? "Photo not posted")
+            }
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Instagram/View Controllers/HomeFeedViewController.swift
+++ Instagram/View Controllers/HomeFeedViewController.swift
@@ -7,29 +7,33 @@
 //
 
 import UIKit
+import Parse
 
-class HomeFeedViewController: UIViewController {
+class HomeFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var feed: [PFObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return feed.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
